2b15710ebc3a5e53f4b3a4a3017ae5bdccd7374c
damdaris <darisadam@upi.edu>
2024-09-19 01:00:28 +0700

    feat: add sound when done

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,18 +7,20 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
     
-    var eggTime: [String: Int] = ["Soft":300, "Medium":420, "Hard":720]
+    var player: AVAudioPlayer?
+    var eggTime: [String: Int] = ["Soft":5, "Medium":420, "Hard":720]
     var timer = Timer()
     
     @IBAction func eggButton(_ sender: UIButton) {
         progressBar.progress = 0.0
-        titleLabel.text = "Egg is cooking..."
+        titleLabel.text = sender.currentTitle
         
         timer.invalidate()
         
@@ -27,15 +29,23 @@ class ViewController: UIViewController {
         
         timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [self] (Timer) in
             if secondsRemaining > 0 {
-                print("\(secondsRemaining) seconds left")
-                secondsRemaining -= 1
                 
-                print(progressBar.progress)
+                secondsRemaining -= 1
+                print("\(secondsRemaining) seconds left")
+                
                 progressBar.progress += 1.0 / Float(eggTime[hardness]!)
+                print(progressBar.progress)
             } else {
                 Timer.invalidate()
                 self.titleLabel.text = "Done!"
+                playSound()
             }
         }
+    }
+    
+    func playSound() {
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player?.play()
     }
 }
